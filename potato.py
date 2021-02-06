28268b7a40979aed9d57b3ee05d61c56c017c6d0
jamesmc <jdbmcclintock@gmail.com>
2021-02-07 12:04:10 +1300

    save point

--- potato.py
+++ potato.py
@@ -5,6 +5,22 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.graph_objs as go
 import pandas as pd
+import plotly.express as px
+
+
+link = ['https://codepen.io/chriddyp/pen/bWlwgP.css']
+app = dash.Dash(__name__, external_stylesheets=link)
+
+
+57
+
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+import pandas as anus
+link = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__,
+                external_stylesheets=link, meta_tags=[{"content": "width=device-width,initial-scale=1"}])
 
 # example measurement stations
 lats = [41.434760, 38.436662]
@@ -15,101 +31,137 @@ def fetch_strong(id, main_df):
     relevant_rows = main_df[main_df["ID"] == id]
     return relevant_rows
 
-app = dash.Dash()
-df = pd.read_csv("with_quake_info.csv")
+df = pd.read_csv("quakes_with_bearings.csv")
+df_dropped_dupes = df.drop_duplicates(subset = "ID")
+
+#layout = go.Layout(margin=dict(t=30, b = 10, l = 10, r = 10))
+
+fig2 = go.Figure(go.Scattermapbox(
+        mode = 'markers',
+        marker = dict(color='black',
+                      size = [i**1.5 * 2 for i in df["pga_size"].tolist()]),
+      ))
+
+
+
+
+fig2.update_layout(
+    height = 450,
+    hovermode='closest',
+    margin = dict(l = 0, r = 0, t = 0, b = 0),
+    mapbox_style="open-street-map",
+    mapbox=dict(
+        bearing=0,
+        center=go.layout.mapbox.Center(
+            lat=-40.2,
+            lon=174.8
+        ),
+        pitch=0,
+        zoom=4.5
+    ))
+
+#print(df)
+
+
+fig = go.Figure(go.Scattermapbox(
+                lat = df_dropped_dupes["EQ_lat"], lon = df_dropped_dupes["EQ_lon"],
+                mode= 'markers',
+                line = dict(color='black', width = 2) ,
+                text= df_dropped_dupes["ID"],
+                marker = dict(color='rgba(255,99,71,0.85)',
+                              size = [i**1.5 * 2 for i in df_dropped_dupes["Magnitude"].tolist()]
+                              ),
+                )
+)
+
+
+fig.update_layout(
+    height = 450,
+    hovermode='closest',
+    margin = dict(l = 0, r = 0, t = 0, b = 0),
+    mapbox_style="open-street-map",
+    mapbox=dict(
+        bearing=0,
+        center=go.layout.mapbox.Center(
+            lat=-40.2,
+            lon=174.8
+        ),
+        pitch=0,
+        zoom=4.5
+    ))
+
+pga_dist_scat = px.scatter(df[0:100], x = 'Epic. Dist.(km)', y = 'max pga')
+pga_dist_scat.update_layout(
+    height = 450,
+    hovermode='closest',
+    margin = dict(l = 0, r = 0, t = 0, b = 0))
+
+polar_plot = px.scatter_polar(df[0:100], r = 'Epic. Dist.(km)', theta = "bearing", size = 'max pga')
+polar_plot.update_layout(
+    height = 450,
+    hovermode='closest',
+    margin = dict(l = 0, r = 0, t = 0, b = 0))
+
 
 app.layout = html.Div([
     html.Div([
-        dcc.Graph(
-            id="mapbox",
-            figure={
-                "data": [
-                    dict(
-                        type="scattermapbox",
-                        lat=df["EQ_lat"],
-                        lon=df["EQ_lon"],
-                        mode="markers",
-                        marker={'size': '14'},
-                        text=df["ID"]
-                    )
-                ],
-                "layout": dict(
-                    autosize=True,
-                    hovermode="closest",
-                    margin=dict(l=0, r=0, t=0, b=0),
-                    mapbox=dict(
-                        #accesstoken=mapbox_access_token,
-                        bearing=0,
-                        center=dict(lat=-40.2,
-                                    lon=174.8),
-                        style="open-street-map",
-                        pitch=0,
-                        zoom=3.5,
-                        layers=[]
-                    )
-                )
-            },
-            style={'width':'45%','height':'80%',
-                   'display': 'inline-block'}
-        )
-    ]),
-    # text container
-    ##"""html.Div([
-      #  html.P(id='station_id',
-       #        style={'fontSize': '12px'})])""",
-    # graph container
+            html.Div([
+                html.H5('Column 1'),
+                dcc.Graph(
+                    id="mapbox",
+                    figure=fig)],
+                    className = 'six columns'),
+            html.Div([html.H6(children='Bananatime'),
+                  dcc.Graph(id='scat',
+                            figure=pga_dist_scat)],
+                 className='six columns')],
+        className="row"),
     html.Div([
-        dcc.Graph(
-            id="mapbox2",
-            figure={
-                "data": [
-                    dict(
-                        type="scattermapbox",
-                        mode="markers",
-                        marker={'size': '14'},
-                        text=text
-                    )
-                ],
-                "layout": dict(
-                    autosize=True,
-                    hovermode="closest",
-                    margin=dict(l=0, r=0, t=0, b=0),
-                    mapbox=dict(
-                        # accesstoken=mapbox_access_token,
-                        bearing=0,
-                        center=dict(lat=-40.2,
-                                    lon=174.8),
-                        style="open-street-map",
-                        pitch=0,
-                        zoom=3.5,
-                        layers=[]
-                    )
-                )
-            }, style={'width':'45%','height':'80%',
-                   'display': 'inline-block'})])
-
-],style =  {"width":"100%"}, className="row")
+            html.Div([
+                html.H5('Column 2'),
+                dcc.Graph(
+                    id="mapbox2",
+                    figure=fig2)],
+                    className = 'six columns'),
+            html.Div([html.H6(children='Bananatime'),
+                  dcc.Graph(id='polar',
+                            figure=polar_plot)],
+                 className='six columns')],
+        className="row"),
+                ])
 
 
-@app.callback(
+
+
+
+@app.callback([
     Output('mapbox2', 'figure'),
+    Output('scat', 'figure')],
     [Input('mapbox', 'clickData')])
 def plot_basin(selection):
     if selection is None:
-        return {}
+        return fig2
     else:
         lat = fetch_strong(selection['points'][0]['text'], df)["Site Latitude"]
         lon = fetch_strong(selection['points'][0]['text'], df)["Site Longitude"]
-
+        strong_color = fetch_strong(selection['points'][0]['text'], df)["color"]
+        strong_size = fetch_strong(selection['points'][0]['text'], df)["pga_size"]
+        max_pga_df = fetch_strong(selection['points'][0]['text'], df)["max pga"]
+        mag_pga_short_string = ["Pga: " + str(i)[0:4] for i in max_pga_df]
+        epic_distance = fetch_strong(selection['points'][0]['text'], df)['Epic. Dist.(km)']
         # depending on the station text use different color for line
 
         data = [go.Scattermapbox(
             lat=lat,
             lon=lon,
-            opacity=0.8,
+            opacity=1,
+            text = mag_pga_short_string,
+            marker=dict(color=strong_color,
+                        size= [(i+ 1)  * 5 for i in strong_size]
+                        ),
             name="Graph"
         )]
-        layout = go.Layout(height = 1000,
+        layout = go.Layout(height = 450,
             hovermode='closest',
             margin = dict(l = 0, r = 0, t = 0, b = 0),
             mapbox_style="open-street-map",
@@ -117,7 +169,7 @@ def plot_basin(selection):
                 bearing=0,
                 center=go.layout.mapbox.Center(
                     lat=-40.2,
-                    lon=174.8
+                    lon= 174.8
                 ),
                 pitch=0,
                 zoom=4.5 ))
